6875bc505c415e41cfb9187bc679e9b748e1fb12
Tarini Prasad Behera <tarini.prasad@rediffmail.com>
2018-08-05 23:57:55 +0800

    initial version

---
+++ src/main/java/com/clinic/management/registry/ClinicManagementRegistryApplication.java
@@ -0,0 +1,14 @@
+package com.clinic.management.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ClinicManagementRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicManagementRegistryApplication.class, args);
+	}
+}
---
+++ src/test/java/com/clinic/management/registry/ClinicManagementRegistryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.clinic.management.registry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClinicManagementRegistryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
